99b95f79a23d38c986f56141b523438f2226cb18
Alexey Artishevskiy <1337kwiz@gmail.com>
2021-12-06 19:33:05 +0300

    Vscode Ctrl+L(vim) + conditional kitty ssh

--- .config/.aliasrc.sh
+++ .config/.aliasrc.sh
@@ -16,8 +16,9 @@ alias lss="lsd --blocks size,name -l -S --size=short -F" # dus lsd variant
 color_lsd=$(ps -p $(ps -p $$ -o ppid=) -o args= | grep nautilus 2>&1 > /dev/null && echo -n "never" || echo -n "always")
 alias ls="lsd -A --group-dirs first --color $color_lsd" \
       find="fd" \
-      grep="rg -i --color always" \
-      ssh="kitty +kitten ssh"
+      grep="rg -i --color always"
+
+[ $(infocmp >/dev/null 2>&1) ] && alias ssh="kitty +kitten ssh"
 
 docker() {
       if [[ $1 == "ps" ]]; then
