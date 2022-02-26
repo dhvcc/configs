878441ba499d0a7bd6571b8bb754ad392d010c46
Alexey <1337kwiz@gmail.com>
2022-02-26 03:33:15 -0800

    Update install.sh

--- .cfg/install.sh
+++ .cfg/install.sh
@@ -14,6 +14,5 @@ if [ $? = 0 ]; then
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 
-config checkout
 config "config" status.showUntrackedFiles no
 config "config" commit.verbose true
