c8aa9d8f7c7293411166407d208090604d67522b
dhvcc <1337kwiz@gmail.com>
2022-07-19 13:17:39 +0300

    Remove useless files

--- .config/gnome-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Setup gnome. Allow backward Alt+Tab with Alt+Shift+Tab and change language with Alt+Shift
-
-gsettings reset org.gnome.desktop.input-sources xkb-options
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
-
