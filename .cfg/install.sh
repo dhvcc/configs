6d8465267816282aaae5e2ca0353ee1d5743c141
dhvcc <alexey.artishevskiy@gmail.com>
2024-01-29 17:06:01 +0300

    Fix order of install.sh and add fallback

--- .cfg/install.sh
+++ .cfg/install.sh
@@ -7,15 +7,15 @@ alias cfg="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 mkdir -p "$HOME/.config-backup"
 git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
 
+cfg checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-    git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
+    cfg checkout
 fi;
-
-cfg checkout
 
 cfg "config" status.showUntrackedFiles no
 cfg "config" commit.verbose true
