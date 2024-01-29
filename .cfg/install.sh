206190f7528f7175501502699aec8935998cd2d6
Alexey <1337kwiz@gmail.com>
2024-01-29 16:55:28 +0300

    WIP fix installer

--- .cfg/install.sh
+++ .cfg/install.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # Origin https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
 
-git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
 alias cfg="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-
-mkdir -p .config-backup
-cfg checkout
+mkdir -p "$HOME/.config-backup"
+git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
 
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
 fi;
+
+cfg checkout
 
 cfg "config" status.showUntrackedFiles no
 cfg "config" commit.verbose true
