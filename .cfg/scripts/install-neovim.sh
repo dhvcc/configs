32d34ed15f0f7125143b69839306b4bfb9f5e000
dhvcc <alexey.artishevskiy@gmail.com>
2024-02-23 19:14:32 +0300

    test

--- .cfg/scripts/install-neovim.sh
+++ .cfg/scripts/install-neovim.sh
@@ -2,10 +2,10 @@
 
 curl -sL -o /var/cache/apt/archives/nvim-linux64.deb  https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb && dpkg -i /var/cache/apt/archives/nvim-linux64.deb
 
-python -m venv ~/.vim/.venv #> /dev/null 2>&1
+python -m venv ~/.vim/.venv
 ~/.vim/.venv/bin/pip install pynvim pyright
-npm i -g neovim #> /dev/null 2>&1
+npm i -g neovim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c ':PlugInstall | :qall'
-#nvim --headless +'PlugInstall' +qall #> /dev/null 2>&1
-#nvim --headless +'CocInstall' +qall  #> /dev/null 2>&1 # FIXME
+#nvim -c ':PlugInstall | :qall'
+#nvim --headless +'PlugInstall' +qall
+#nvim --headless +'CocInstall' +qall # FIXME
