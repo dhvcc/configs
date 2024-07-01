bf148ab1401cbecc6f5751cb79bf67c96e9f368a
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-02 01:00:51 +0300

    Update backup mechanism

--- .cfg/install.sh
+++ .cfg/install.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-# Origin https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
+# Inspiration https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
 
 cd "$HOME"
 
 alias cfg="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 mkdir -p "$HOME/.config-backup"
-git clone --bare https://github.com/dhvcc/configs.git $HOME/.cfg
+git clone --no-checkout --bare https://github.com/dhvcc/configs.git $HOME/.cfg
+
+# Backup and remove repo files (if exist)
+cfg ls-tree -r master --name-only | xargs -I {} mv {} ./.config-backup/{}
 
 cfg checkout
-
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-    cfg checkout
-fi;
 
 cfg "config" status.showUntrackedFiles no
 cfg "config" commit.verbose true
