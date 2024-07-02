37e749d10633d5ed8da1eb32b66539db8de87db2
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-02 18:18:16 +0300

    fix neovim install

--- .cfg/scripts/install-neovim.sh
+++ .cfg/scripts/install-neovim.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
-. ~/.zshrc
+export PATH="/home/linuxbrew/.linuxbrew/bin:/opt/homebrew/bin:$PATH"
 
 curl -fLo '~/.vim/autoload/plug.vim' --create-dirs \
     'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-python -m venv ~/.vim/.venv
-~/.vim/.venv/bin/pip install pynvim pyright
-npm i -g neovim
+#python -m venv ~/.vim/.venv
+#~/.vim/.venv/bin/pip install pynvim pyright
+#npm i -g neovim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +'PlugInstall' +qall
-nvim --headless +'CocInstall' +qall  # FIXME
+#nvim --headless +'CocInstall' +qall  # FIXME
