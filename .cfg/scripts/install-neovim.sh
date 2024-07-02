0d8eae26e5d2dac99b0412f963eb3aa55b5ca552
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-03 01:51:07 +0300

    Try to fix install in docker

--- .cfg/scripts/install-neovim.sh
+++ .cfg/scripts/install-neovim.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
-curl -fLo '~/.vim/autoload/plug.vim' --create-dirs \
-    'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim --headless +'PlugInstall' +qall
 nvim --headless "+Lazy! sync" +qa
