c5c34b9c21f9d0974a63f0f9062c12f48fa4ef38
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-03 02:01:49 +0300

    Try to fix neovim install lmao

--- .cfg/scripts/install-neovim.sh
+++ .cfg/scripts/install-neovim.sh
@@ -6,7 +6,7 @@ nvim --headless +'PlugInstall' +qall
 nvim --headless "+Lazy! sync" +qa
 nvim --headless "+MasonInstall dockerfile-language-server" +qa
 nvim --headless "+MasonInstall docker-compose-language-service" +qa
-nvim --headless "+MasonInstall pyright" +qa
+nvim --headless "+MasonInstall --force pyright" +qa
 nvim --headless "+MasonInstall gopls" +qa
 nvim --headless "+MasonInstall typescript-language-server" +qa
 nvim --headless "+MasonInstall ansible-language-server" +qa
