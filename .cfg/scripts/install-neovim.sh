d3896f1521bc83a60580095b98a8e7eff7c694ba
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-03 01:34:55 +0300

    Try to fix install in docker, cleanup

--- .cfg/scripts/install-neovim.sh
+++ .cfg/scripts/install-neovim.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-export PATH="/home/linuxbrew/.linuxbrew/bin:/opt/homebrew/bin:$PATH"
-
 curl -fLo '~/.vim/autoload/plug.vim' --create-dirs \
     'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
--- .cfg/scripts/install-omz.sh
+++ .cfg/scripts/install-omz.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env zsh
 
-export ZSH="$HOME/.oh-my-zsh"
-export ZSH_CUSTOM="$HOME/.oh-my-zsh-custom"
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#d1d1d1,bg=#525252"
-fpath+="$ZSH_CUSTOM/plugins/zsh-completions/src"
-source $ZSH/oh-my-zsh.sh
-
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-completions "$HOME/.oh-my-zsh/custom/plugins/zsh-completions"
