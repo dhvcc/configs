4eb78701b2c59a48122cbf7aa0e177873a5aa31d
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-03 03:47:11 +0300

    Disable supermaven by default, fix telescope rg

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,30 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    pickers = {
+      find_files = {
+        find_command = {
+          'rg',
+          '--ignore-vcs',
+          '--hidden',
+          '--files',
+          '--glob=!.git',
+          '--glob=!venv',
+          '--glob=!.venv',
+          '--glob=!.cache',
+          '--glob=!venvs',
+          '--glob=!virtualenv',
+          '--glob=!virtualenvs',
+          '--glob=!site-packages',
+          '--glob=!__pycache__',
+          '--glob=!node_modules',
+          '--glob=!.vscode-server',
+        },
+      },
+    },
+    defaults = {
+      prompt_prefix = '=> ',
+    },
+  }
 }
