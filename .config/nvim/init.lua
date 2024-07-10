1ad86fe3aa17fec2239b5b83ec1d088d67d348a8
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-11 02:59:01 +0300

    Fix toggleterm T mapping to n, add cd to telescope, cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,26 @@
-vim.opt.runtimepath:prepend("~/.vim")
-vim.opt.runtimepath:append("~/.vim/after")
+-- vim.opt.runtimepath:prepend("~/.vim")
+-- vim.opt.runtimepath:append("~/.vim/after")
+
+vim.api.nvim_set_keymap('n', '<C-_>', 'gcc', {} )
+vim.api.nvim_set_keymap('v', '<C-_>', 'gc', {} )
 
 vim.cmd("source ~/.vimrc")
+-- vim.opt.clipboard = "unnamedplus"
+-- TODO: fix this
+-- if vim.fn.has("wsl") == 1 then
+--     vim.g.clipboard = {
+--         name = "WSL clipboard",
+--         copy = {
+--             ["+"] = "clip.exe",
+--             ["*"] = "clip.exe",
+--         },
+--         paste = {
+--       ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--             ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+-- 
+--         },
+--         cache_enabled = true,
+--     }
+-- end
 
 require("config.lazy")
