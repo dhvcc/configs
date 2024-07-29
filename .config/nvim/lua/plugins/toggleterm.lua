bc64ff7dc484611cea993c9769cea9ec98e9d575
dhvcc <alexey.artishevskiy@gmail.com>
2024-07-29 22:42:16 +0300

    Make toggleterm horizontal

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -11,17 +11,17 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = 'float',
+      direction = 'horizontal',
       close_on_exit = true,
       shell = vim.o.shell,
-      float_opts = {
-        border = 'curved',
-        winblend = 0,
-        highlights = {
-          border = 'Normal',
-          background = 'Normal',
-        },
-      },
+      -- float_opts = {
+      --   border = 'curved',
+      --   winblend = 0,
+      --   highlights = {
+      --     border = 'Normal',
+      --     background = 'Normal',
+      --   },
+      -- },
     }
   end,
   keys = {
