771e5132c6e731d2fde61f50e9dab8dacf26af1f
dhvcc <alexey.artishevskiy@gmail.com>
2026-05-29 17:01:34 +0200

    ignore mise by default

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,123 +1,123 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vscode",
-    },
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      indent = {
-        enabled = false,
-      },
-    },
-  },
-  {
-    "rafamadriz/friendly-snippets",
-    enabled = false,
-  },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      snippets = {
-        preset = "vsnip",
-        expand = function() end,
-        active = function()
-          return false
-        end,
-        jump = function() end,
-      },
-      sources = {
-        default = { "lsp", "path", "buffer" },
-        transform_items = function(_, items)
-          return vim.tbl_filter(function(item)
-            return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
-          end, items)
-        end,
-      },
-      keymap = {
-        preset = "default",
-        ["<Tab>"] = { "select_and_accept", "fallback" },
-        ["<S-Tab>"] = { "fallback" },
-        ["<CR>"] = false,
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ["*"] = {
-          capabilities = {
-            textDocument = {
-              completion = {
-                completionItem = {
-                  snippetSupport = false,
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      close_if_last_window = true,
-      window = {
-        width = 30,
-      },
-      buffers = {
-        follow_current_file = true,
-      },
-      filesystem = {
-        follow_current_file = true,
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            "node_modules",
-          },
-          never_show = {
-            ".DS_Store",
-            "thumbs.db",
-          },
-        },
-      },
-    },
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    },
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    opts = {
-      direction = "horizontal",
-      shade_terminals = false,
-    },
-    keys = {
-      { "<leader>j", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle terminal" },
-    },
-  },
-  {
-    "folke/flash.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, false },
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "vscode",
+		},
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = {
+				enabled = false,
+			},
+		},
+	},
+	{
+		"rafamadriz/friendly-snippets",
+		enabled = false,
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			snippets = {
+				preset = "vsnip",
+				expand = function() end,
+				active = function()
+					return false
+				end,
+				jump = function() end,
+			},
+			sources = {
+				default = { "lsp", "path", "buffer" },
+				transform_items = function(_, items)
+					return vim.tbl_filter(function(item)
+						return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
+					end, items)
+				end,
+			},
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "select_and_accept", "fallback" },
+				["<S-Tab>"] = { "fallback" },
+				["<CR>"] = false,
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				["*"] = {
+					capabilities = {
+						textDocument = {
+							completion = {
+								completionItem = {
+									snippetSupport = false,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			close_if_last_window = true,
+			window = {
+				width = 30,
+			},
+			buffers = {
+				follow_current_file = true,
+			},
+			filesystem = {
+				follow_current_file = true,
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						"node_modules",
+					},
+					never_show = {
+						".DS_Store",
+						"thumbs.db",
+					},
+				},
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			current_line_blame = true,
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			direction = "horizontal",
+			shade_terminals = false,
+		},
+		keys = {
+			{ "<leader>j", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle terminal" },
+		},
+	},
+	{
+		"folke/flash.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, false },
+		},
+	},
 }
